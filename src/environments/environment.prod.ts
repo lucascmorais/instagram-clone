fcbd967f340bd4e02a5f6b1c3f34f8dee36d5344
Lucas Costa Morais <lucas.cmorais@hotmail.com>
2021-02-04 17:58:41 -0300

    Update deploy in app engine

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC3MnfO0-2G2bE1p9WRBBmsBqVvDUXbqyw",
+    authDomain: "clone-do-instagram-angular.firebaseapp.com",
+    projectId: "clone-do-instagram-angular",
+    storageBucket: "clone-do-instagram-angular.appspot.com",
+    messagingSenderId: "738084758249",
+    appId: "1:738084758249:web:01243af8df149c1a338678",
+    measurementId: "G-P4RVHSQ60W"
+  }
 };
